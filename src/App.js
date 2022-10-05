810088b931287a7a8cdce31db370c3085c7f2c6b
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-05 18:01:20 +0200

    Clean up some divs

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import "./main.css";
 
 function App() {
   return (
-    <div>
-      <div>
-        <div className="header-wrapper background-rounded">
-          <div className="logo-wrapper">
-            <h3>Er</h3>
-            <div className="icons-wrapper">
-              <p>Git</p>
-              <p>LinkedIn</p>
-              <p>Contact</p>
-            </div>
+    <div className="main">
+      <div className="header-wrapper background-rounded">
+        <div className="logo-wrapper">
+          <h3>Er</h3>
+          <div className="icons-wrapper">
+            <p>Git</p>
+            <p>LinkedIn</p>
+            <p>Contact</p>
           </div>
-          <div>Clock</div>
-          <div>Settings</div>
         </div>
+        <div>Clock</div>
+        <div>Settings</div>
       </div>
       <div className="sidebar-wrapper background-rounded">
         <div className="sidebar-top">
