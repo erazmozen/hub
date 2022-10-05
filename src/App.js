8d3fbfc927a5aaba85a2b2a3c8086b3105490afa
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-05 18:25:06 +0200

    Added icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import "./main.css";
+import { FiGithub } from "react-icons/fi";
+import { GrLinkedinOption } from "react-icons/gr";
+import { FiBookOpen } from "react-icons/fi";
+import { FiSettings } from "react-icons/fi";
 
 function App() {
   return (
@@ -7,13 +11,16 @@ function App() {
         <div className="logo-wrapper">
           <h3>Er</h3>
           <div className="icons-wrapper">
-            <p>Git</p>
-            <p>LinkedIn</p>
-            <p>Contact</p>
+            <FiGithub />
+            <GrLinkedinOption />
+            <FiBookOpen />
           </div>
         </div>
         <div>Clock</div>
-        <div>Settings</div>
+        <div className="header-settigns">
+          <p>Settings</p>
+          <FiSettings size={18} />
+        </div>
       </div>
       <div className="sidebar-wrapper background-rounded">
         <div className="sidebar-top">
