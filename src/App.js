cd64a7f3369ce857086082f5e5872fca6b2d2e5f
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-05 17:21:08 +0200

    Added basic markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { MdAccessibilityNew } from "react-icons/md";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <div>
+      <div className="header-wrapper">
+        <div>
+          <div>
+            <h3>Er</h3>
+            <div>
+              <p>Git</p>
+              <p>LinkedIn</p>
+              <p>Contact</p>
+            </div>
+          </div>
+          <div>Clock</div>
+          <div>Settings</div>
+        </div>
+      </div>
+      <div>Sidebar</div>
+      <div>Footer</div>
+    </div>
+  );
 }
 
 export default App;
