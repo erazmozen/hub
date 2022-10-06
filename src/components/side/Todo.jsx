e7ceec78a5f494c8f68e2bcee8d43998db0de7d9
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-06 17:18:10 +0200

    Removing todo with the same id as clicked

--- src/components/side/Todo.jsx
+++ src/components/side/Todo.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 
 const Todo = ({ icons, searchInput }) => {
   const [todos, setTodos] = useState([
-    { title: "Lets put some" },
-    { title: "Random text here" },
-    { title: "To show some todos" },
+    { id: 0, title: "Lets put some" },
+    { id: 1, title: "Random text here" },
+    { id: 2, title: "To show some todos" },
   ]);
 
   function addRandomTodo() {
     console.log("add random todo");
     const todoTitle = searchInput.current.value;
     if (todoTitle === "") return;
-    setTodos((prev) => [...prev, { title: todoTitle }]);
+    setTodos((prev) => [...prev, { id: todos.length, title: todoTitle }]);
   }
+
+  const deleteTodo = (index) => {
+    const todosFiltered = [...todos].filter((todo) => todo.id !== index);
+    console.log(todosFiltered);
+    setTodos(todosFiltered);
+  };
 
   return (
     <div className="todo">
@@ -23,7 +29,9 @@ const Todo = ({ icons, searchInput }) => {
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
-            <icons.FiMinusSquare className="minus-icon" />
+            <button onClick={() => deleteTodo(todo.id)}>
+              <icons.FiMinusSquare className="minus-icon" />
+            </button>
             <h4 className="todo-title">{todo.title}</h4>
           </li>
         ))}
