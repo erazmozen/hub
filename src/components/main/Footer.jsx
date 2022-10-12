d6e1de0b7a0bd0bbffdb1aebfed12142ef59ab29
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-12 06:51:45 +0200

    Fill footer with some info

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,7 +2,13 @@ import "../css/main/footer.css";
 
 const Footer = () => {
   console.log("Footer render");
-  return <div className="footer-wrapper background-rounded">Footer</div>;
+  return (
+    <div className="footer-wrapper background-rounded">
+      <p>Contact phone: +38163444335</p>
+      <p>Email: erazmo@erazmo.com</p>
+      <p>Copyright by: ErazmoZen</p>
+    </div>
+  );
 };
 
 export default Footer;
