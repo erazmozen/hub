65e46cb9ca7b21b2e71ed5e7f75eccb8d726d923
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-14 10:21:13 +0200

    Handle counts

--- src/components/side/Poll.jsx
+++ src/components/side/Poll.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
 import "../css/side/poll.css";
 
 const Poll = () => {
+  const [poll, setPoll] = useState({ yes: 8351231236, no: 2573, voted: null });
+
+  function changeYesPoll() {
+    if (poll.voted) return;
+    setPoll((prev) => ({ ...prev, yes: prev.yes + 1, voted: 1 }));
+  }
+
+  function changeNoPoll() {
+    if (poll.voted) return;
+    setPoll((prev) => ({ ...prev, no: prev.no + 1, voted: 1 }));
+  }
+
   return (
     <div className="poll-wrapper">
       <div className="question-wrapper">
@@ -14,15 +27,19 @@ const Poll = () => {
       </div>
       <div className="vote-wrapper">
         <div className="yes-bar">
-          <h2>Yes</h2>
-          <h3>yesCount ( 8351231236 )</h3>
+          <h2 onClick={changeYesPoll}>Yes</h2>
+          <h3>yesCount ( {poll.yes} )</h3>
         </div>
         <div className="no-bar">
-          <h2>No</h2>
-          <h3>noCount ( 2573 )</h3>
+          <h2 value="no" onClick={changeNoPoll}>
+            No
+          </h2>
+          <h3>noCount ( {poll.no} )</h3>
         </div>
       </div>
-      <h3>Thanks for voting!</h3>
+
+      {!poll.voted && <h3>U can vote only once!</h3>}
+      {poll.voted && <h3>Thanks for voting!</h3>}
     </div>
   );
 };
