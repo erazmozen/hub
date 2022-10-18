21eb6e11e657ac6bcf9f5be3ebd6d9ef650ff74f
erazmozen <erazmozen@protonmail.com>
2022-10-18 10:46:11 +0200

    Handle resize buttons

--- src/components/side/Notes.jsx
+++ src/components/side/Notes.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import "../css/side/notes.css";
 
 const Notes = ({ icons }) => {
+  const [bodyHeight, setBodyHeight] = useState("200px");
+  const titleInput = useRef();
+  const bodyInput = useRef();
+
+  const heightClass = {
+    transition: "height 0.3s ease",
+    height: `${bodyHeight}`,
+  };
+
   const notes = [
-    { id: 1212, title: "title1", body: "body1", date: "23.21.1222" },
-    { id: 12312, title: "title2", body: "body2", date: "23.21.1222" },
-    { id: 12512, title: "title3", body: "body3", date: "23.21.1222" },
+    { id: 122312, title: "title1", body: "body", date: "2321122" },
+    { id: 123212, title: "title", body: "body", date: "2321122" },
+    { id: 1231312, title: "title", body: "body", date: "2321122" },
   ];
 
   const buttons = [
@@ -25,30 +35,59 @@ const Notes = ({ icons }) => {
     },
   ];
 
+  function changeHeight(e) {
+    e.preventDefault();
+    // setBodyHeight(e.target.value);
+    if (bodyHeight === e.target.value) {
+      console.log("Value is same, exiting function...");
+      return;
+    }
+    setBodyHeight(() => e.target.value);
+    console.log("Value is not the same, setting state to ", e.target.value);
+  }
+
+  console.log("Notes render");
+
   return (
     <div className="notes">
       <form>
-        <input placeholder="Title" className="common-input"></input>
+        <input
+          ref={titleInput}
+          placeholder="Title"
+          className="common-input"
+        ></input>
+
         <div className="toggles-wrapper">
           {buttons.map((button) => (
             <button
               key={button.id}
               value={button.value}
               className="common-button"
+              onClick={changeHeight}
             >
               {button.icon}
             </button>
           ))}
+
           <button className="common-button">
             <icons.AiOutlineClear size={18} />
           </button>
         </div>
-        <textarea placeholder="Body" className="common-input"></textarea>
+
+        <textarea
+          ref={bodyInput}
+          placeholder="Body"
+          className={`common-input textarea-body`}
+          style={heightClass}
+        />
 
         <button className="common-button" type="submit">
           Save
         </button>
       </form>
+
+      <h1 style={{ marginTop: "1.4rem" }}>Saved notes:</h1>
+
       <div className="notes-wrapper">
         {notes.map((note) => (
           <div key={note.id} className="note">
