59cebd2208399f5c2fdaa37ccea8856871173bf4
erazmozen <erazmozen@protonmail.com>
2022-10-18 20:24:30 +0200

    Remove projects from showpage

--- src/components/main/ShowPage.jsx
+++ src/components/main/ShowPage.jsx
@@ -1,5 +1,4 @@
 import Portfolio from "../showpage/Portfolio";
-import Projects from "../showpage/Projects";
 import Notes from "../side/Notes";
 import Poll from "../side/Poll";
 import ProfileShowpage from "../side/ProfileShowpage";
@@ -9,7 +8,6 @@ const ShowPage = ({ icons }) => {
   return (
     <div className="background-rounded showpage">
       <ProfileShowpage icons={icons} />
-      <Projects icons={icons} />
       <Poll />
       <Portfolio icons={icons} />
       <Notes icons={icons} />
