9b160053d0e0db35dfc3ba0eed82362eede9a8bd
erazmozen <erazmozen@protonmail.com>
2022-10-20 07:40:21 +0200

    Added preview images

---
+++ src/data/prevData.js
@@ -0,0 +1,21 @@
+import calcPrev from "../imgprev/calc-prev-min.jpg";
+import fitPrev from "../imgprev/fitnes-prev-min.jpg";
+import genPrev from "../imgprev/gen-prev-min.jpg";
+import logPrev from "../imgprev/login-prev-min.jpg";
+import memePrev from "../imgprev/meme-prev-min.jpg";
+import portPrev from "../imgprev/portfolio-prev-min.jpg";
+import slidePrev from "../imgprev/slider-prev-min.jpg";
+import tenzPrev from "../imgprev/tenzies-prev-min.jpg";
+
+const prevImages = [
+  calcPrev,
+  fitPrev,
+  genPrev,
+  logPrev,
+  memePrev,
+  portPrev,
+  slidePrev,
+  tenzPrev,
+];
+
+export default prevImages;
