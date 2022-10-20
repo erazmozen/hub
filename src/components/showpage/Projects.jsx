0ce59f49cd3d945eea02c61dec0a92001dbb6d9b
erazmozen <erazmozen@protonmail.com>
2022-10-20 12:44:36 +0200

    Update Projects.jsx
    Map over links to get ids for matching
    Use State to implement slider for links

--- src/components/showpage/Projects.jsx
+++ src/components/showpage/Projects.jsx
@@ -2,22 +2,80 @@ import "../css/showpage/showpage.css";
 import ProjectLink from "../side/ProjectLink";
 import ProjectPreview from "../side/ProjectsPreview";
 import prevImages from "../../data/prevData";
+import { useState } from "react";
 
 const Projects = ({ icons }) => {
+  const [index, setIndex] = useState(0);
+
+  const projectLinks = [
+    {
+      title: "Fitnes-App",
+    },
+    {
+      title: "React-Signin",
+    },
+    {
+      title: "Portfolio-Template",
+    },
+    {
+      title: "React-Calculator",
+    },
+    {
+      title: "Meme-Generator",
+    },
+    {
+      title: "React-ImageSlider",
+    },
+    {
+      title: "React-Note-App",
+    },
+    {
+      title: "Password-Generator",
+    },
+  ];
+
+  function changeSlide(linkIndex) {
+    console.log("changeSlide");
+    if (linkIndex === index) return;
+    setIndex(linkIndex);
+  }
+
+  function nextSlide() {
+    const isLast = index === prevImages.length - 1;
+    setIndex(isLast ? 0 : index + 1);
+  }
+
+  function prevSlide() {
+    const isFirst = index === 0;
+    setIndex(isFirst ? prevImages.length - 1 : index - 1);
+  }
+
   console.log("Projects render");
   return (
     <div className="projects">
-      <div className="slink-holder">
-        <ProjectLink icons={icons} title="Fitnes-App" />
-        <ProjectLink icons={icons} title="React-Gallery" />
-        <ProjectLink icons={icons} title="Portfolio-Template" />
-        <ProjectLink icons={icons} title="React-Calculator" />
-        <ProjectLink icons={icons} title="Meme-Generator" />
-        <ProjectLink icons={icons} title="React-ImageSlider" />
-        <ProjectLink icons={icons} title="React-Note-App" />
-        <ProjectLink icons={icons} title="Password-Generator" />
+      <div className="link-holder">
+        {projectLinks.map((link, linkIndex) => (
+          <ProjectLink
+            icons={icons}
+            index={index}
+            linkIndex={linkIndex}
+            changeSlide={changeSlide}
+            title={link.title}
+          />
+        ))}
+
+        <div>
+          <div className="button-holder">
+            <button className="navigate-button" onClick={prevSlide}>
+              <icons.MdOutlineNavigateBefore />
+            </button>
+            <button className="navigate-button" onClick={nextSlide}>
+              <icons.MdOutlineNavigateNext />
+            </button>
+          </div>
+        </div>
       </div>
-      <ProjectPreview prevImages={prevImages} />
+      <ProjectPreview index={index} prevImages={prevImages} />
     </div>
   );
 };
