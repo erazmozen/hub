4eae8f7c39f2517eddb854f27a97442cf1ed3056
erazmozen <erazmozen@protonmail.com>
2022-10-22 10:32:01 +0200

    Render weather widget only if data avaleable

--- src/components/Showpage/ShowPage.jsx
+++ src/components/Showpage/ShowPage.jsx
@@ -3,12 +3,18 @@ import Notes from "./comps/Notes";
 import Poll from "./comps/Poll";
 import ShowpageHeader from "./comps/ShowpageHeader";
 import "./showpage.css";
+import Weather from "./comps/Weather";
+import { useContext } from "react";
+import DataContext from "../../contexts/DataContext";
 
 const ShowPage = ({ icons }) => {
+  const loading = useContext(DataContext);
+  const isLoading = loading.loading;
   console.log("ShowPage render");
   return (
     <div className="background-rounded showpage">
       <ShowpageHeader icons={icons} />
+      {!isLoading && <Weather icons={icons} />}
       <Portfolio icons={icons} />
       <Notes icons={icons} />
       <Poll />
