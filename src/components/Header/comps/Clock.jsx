5067b95d7efe4f58d535e577d98c501417b87704
erazmozen <erazmozen@protonmail.com>
2022-10-25 12:37:12 +0200

    Make clock more custom

---
+++ src/components/Header/comps/Clock.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./clock.css"
+
+const Clock = ({ icons }) => {
+    const [clockState, setClockState] = useState({ loading: true });
+
+    useEffect(() => {
+
+        const getTimeData = async () => {
+            const date = new Date();
+            const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+            const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
+            setClockState({
+                month: month,
+                day: day,
+                year: year,
+                hour: hour,
+                minutes: minutes,
+                seconds: seconds,
+                loading: false
+            });
+        }
+
+        setInterval(() => {
+            getTimeData();
+        }, 60000);
+    }, []);
+
+    if (setClockState == {}) console.log('nula!')
+
+    return (
+        <div>
+            {
+                !clockState.loading
+                &&
+                <div className="clock-wrapper">
+                    <div className="clock-time">
+                        <p>{clockState.hour}:{clockState.minutes}</p>
+                        <icons.CgTimer />
+                    </div>
+                    <div className="clock-date">
+                        <p>{clockState.month}.{clockState.day}.{clockState.year}.</p>
+                        <icons.CgCalendarDates />
+                    </div>
+
+                </div>
+            }
+        </div>
+    );
+};
+
+export default Clock
