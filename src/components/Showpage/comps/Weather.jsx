62ad0f5feefb07a3bf1dac7cee00a4f133ac130b
Erazmo Zen <67731959+erazmozen@users.noreply.github.com>
2022-10-25 11:15:36 +0200

    Move component to its own file
    Also change api key a bit

--- src/components/Showpage/comps/Weather.jsx
+++ src/components/Showpage/comps/Weather.jsx
@@ -1,42 +1,22 @@
 import React, { useContext } from "react";
 import "./weather.css";
 import useApi from "../../common/hooks/useApi";
+import WeatherContainer from "./WeatherContainer";
 
 const Weather = ({ icons }) => {
   const { loading, data } = useApi(
-    "https://api.open-meteo.com/v1/forecast?latitude=44.43&longitude=20.64&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,direct_radiation,diffuse_radiation&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&current_weather=true&timezone=auto"
+    "https://api.open-meteo.com/v1/forecast?latitude=44.43&longitude=20.64&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&current_weather=true&timezone=auto"
   );
 
-  const WeatherContainer = ({ data, icons }) => {
-    const weatherDataObject = {
-      city: `Mladenovac, Koracica`,
-      cords: `${data.latitude} ${data.longitude}`,
-      currTemp: `Currently: ${data.current_weather.temperature}℃`,
-    };
-
+  if (data == null) {
     return (
-      <div className="card weather-wrapper">
-        <div className="weather-header">
-          <div className="weather-title">
-            {weatherDataObject.intervalIcon}
-            <h2>Weather Widget</h2>
-          </div>
-          <h3 className="weather-city">{weatherDataObject.city}</h3>
-          <h4 className="weather-cords">
-            <icons.TbCurrentLocation size={13} />
-            {weatherDataObject.cords}
-          </h4>
-        </div>
-        <div className="wather-body">
-          <h2>{weatherDataObject.currTemp}</h2>
-        </div>
-      </div>
-    );
-  };
+      <div>Data is unavaliable!</div>
+    )
+  }
 
-  console.log("Weather render");
+  console.log("Weather render", data);
 
-  return !loading && <WeatherContainer icons={icons} data={data} />;
+  return <WeatherContainer icons={icons} data={data} />;
 };
 
 export default Weather;
---
+++ src/components/Showpage/comps/WeatherContainer.jsx
@@ -0,0 +1,31 @@
+const WeatherContainer = ({ data, icons }) => {
+
+    console.log("inside comp", data)
+
+    const weatherDataObject = {
+        city: `Mladenovac, Koracica`,
+        cords: `${data.latitude} ${data.longitude}`,
+        currTemp: `Currently: ${data.current_weather.temperature}℃`,
+    };
+
+    return (
+        <div className="card weather-wrapper">
+            <div className="weather-header">
+                <div className="weather-title">
+                    {weatherDataObject.intervalIcon}
+                    <h2>Weather Widget</h2>
+                </div>
+                <h3 className="weather-city">{weatherDataObject.city}</h3>
+                <h4 className="weather-cords">
+                    <icons.TbCurrentLocation size={13} />
+                    {weatherDataObject.cords}
+                </h4>
+            </div>
+            <div className="wather-body">
+                <h2>{weatherDataObject.currTemp}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default WeatherContainer
