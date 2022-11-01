c7086f9aedcdeab5a75bc89648728fb85ddf22b0
erazmozen <erazmozen@protonmail.com>
2022-11-01 18:07:34 +0100

    Clear comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,8 @@
 import "./header.css";
-// import { useState } from "react";
-// import Popup from "./comps/Popup";
 import Clock from "./comps/Clock";
 import DataContext from "../../contexts/DataContext";
 
 const Header = ({ icons }) => {
-  // const [togglePopup, setTogglePopup] = useState(false);
-
-  // function togglePopupFunction() {
-  //   setTogglePopup((prev) => (prev = !prev));
-  // }
-
   console.log("Header render");
 
   return (
@@ -29,12 +21,6 @@ const Header = ({ icons }) => {
           <icons.FiBookOpen />
         </div>
       </div>
-
-      {/* <div onClick={togglePopupFunction} className="header-settigns">
-        <p>Settings</p>
-        <icons.FiSettings />
-        {togglePopup && <Popup icons={icons} />}
-      </div> */}
 
       <Clock icons={icons} />
     </div>
