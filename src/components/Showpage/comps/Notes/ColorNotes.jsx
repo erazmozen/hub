d09414bc8973100978fda6d5b7a978ec5d4a86f5
erazmozen <erazmozen@protonmail.com>
2022-11-02 16:29:37 +0100

    Added color picker for notes

---
+++ src/components/Showpage/comps/Notes/ColorNotes.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const ColorNotes = ({ note, changeNoteColor }) => {
+  const [noteColor, setNoteColor] = useState(note.color);
+
+  const colorArray = [
+    "#282a36",
+    "#44475a",
+    "#6272a4",
+    "#8be9fd",
+    "#50fa7b",
+    "#ffb86c",
+    "#ff79c6",
+    "#bd93f9",
+    "#ff5555",
+    "#f1fa8c",
+  ];
+
+  const style = {
+    background: noteColor,
+  };
+
+  function changeColor(e) {
+    setNoteColor(e.target.name);
+    changeNoteColor(note.id, e.target.name);
+  }
+
+  console.log("Color Notes render");
+  return (
+    <div className="color-notes">
+      <button className="color-bar" style={style}></button>
+      <div className="button-wrapper">
+        {colorArray.map((color) => (
+          <button
+            name={color}
+            onClick={changeColor}
+            className="color-bar color-bar-toggle"
+            style={{ background: color }}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ColorNotes;
