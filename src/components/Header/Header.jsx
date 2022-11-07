eceb1143d028a41974df5e3331935692e89d39b5
erazmozen <erazmozen@protonmail.com>
2022-11-07 13:24:12 +0100

    Use hook to get context functions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,32 @@
 import "./header.css";
 import Clock from "./comps/Clock";
+import { useContext } from "react";
 import DataContext from "../../contexts/DataContext";
 
 const Header = ({ icons }) => {
+  const { toggleSidebar, toggleShowpage } = useContext(DataContext);
+
   console.log("Header render");
 
   return (
-    <div className="header-wrapper background-rounded">
-      <div className="logo-wrapper">
-        <DataContext.Consumer>
-          {(functions) => (
-            <button onClick={functions.toggleSidebar}>
-              <icons.BsToggles />
-            </button>
-          )}
-        </DataContext.Consumer>
-        <div className="icons-wrapper">
-          <icons.GrLinkedinOption />
-          <icons.FiGithub />
+    <div className="header-wrapper card">
+      <div className="common-icons-wrapper">
+        <button onClick={toggleSidebar}>
+          <icons.BsToggles />
+        </button>
+        <button onClick={toggleShowpage}>
+          <icons.BsReverseLayoutTextSidebarReverse />
+        </button>
+        <a href="#">
           <icons.FiBookOpen />
-        </div>
+        </a>
+        <a href="#">
+          <icons.GrLinkedinOption />
+        </a>
+        <a href="#">
+          <icons.FiGithub />
+        </a>
       </div>
-
       <Clock icons={icons} />
     </div>
   );
