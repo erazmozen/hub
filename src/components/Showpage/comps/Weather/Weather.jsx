ff86c735d24539e3c6ea4ff9028d997f06725862
erazmozen <erazmozen@protonmail.com>
2022-11-29 08:41:45 +0100

    Some style for picker

--- src/components/Showpage/comps/Weather/Weather.jsx
+++ src/components/Showpage/comps/Weather/Weather.jsx
@@ -6,6 +6,7 @@ import weatherCityData from "../../../../data/weatherCityData.json";
 
 const Weather = ({ icons }) => {
   const [city, setCity] = useState(weatherCityData[0]);
+  const [searchInput, setSearchInput] = useState("");
 
   const { loading, data } = useApi(
     `https://api.open-meteo.com/v1/forecast?latitude=${city.Latitude}&longitude=${city.Longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&current_weather=true&timezone=auto`,
@@ -13,20 +14,30 @@ const Weather = ({ icons }) => {
   );
 
   function changeCity(index) {
-    console.log(index);
     setCity(weatherCityData[index]);
   }
 
   if (data == null) {
     return <div>Data is unavaliable!</div>;
   }
-
-  console.log("Weather render", city.Latitude);
+  const filterCity = weatherCityData.filter(
+    (city) =>
+      city.Capital.toLowerCase().includes(searchInput) ||
+      city.Country.toLowerCase().includes(searchInput)
+  );
+  console.log("Weather render");
 
   return (
     <div>
+      <button className="common-button">Choose city</button>
+      <input
+        typeof="text"
+        className="common-input"
+        placeholder="search.."
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
       <div className="city-wrapper">
-        {weatherCityData.map((city, index) => (
+        {filterCity.map((city, index) => (
           <div key={index} className="city-picker" onClick={() => changeCity(index)}>
             <p>{city.Country}</p>
             <p>~</p>
