c6af63b70376d16dfa8d25f55d0c0da0d87c36bb
erazmozen <erazmozen@protonmail.com>
2022-12-01 13:45:04 +0100

    Cleanup

--- src/components/Showpage/Content.jsx
+++ src/components/Showpage/Content.jsx
@@ -4,9 +4,7 @@ import Sidebar from "../Sidebar/Sidebar";
 import ShowPage from "../Showpage/ShowPage";
 
 const Content = ({ icons }) => {
-  const contextValue = useContext(TogglesContext);
-  const { toggleSidebar, toggleShowpage, sidebarToggle, showpageToggle } =
-    useContext(TogglesContext);
+  const { sidebarToggle, showpageToggle } = useContext(TogglesContext);
   console.log("Content Render");
   return (
     <div className="content-wrapper">
