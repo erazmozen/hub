ab5cc34807350a98fa93a7d854c30582f0acf293
erazmozen <erazmozen@protonmail.com>
2022-12-01 13:38:33 +0100

    Reorganize structure and move context
    Context provided by TogglesContext file now

--- src/App.js
+++ src/App.js
@@ -1,43 +1,19 @@
-import { useState } from "react";
 import "./main.css";
 import icons from "./icons";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
-import ShowPage from "./components/Showpage/ShowPage";
-import DataContext from "./contexts/DataContext";
+import { TogglesContextProvider } from "./contexts/TogglesContext";
+import Content from "./components/Showpage/Content";
 
 function App() {
-  const [sidebarToggle, setSidebarToggle] = useState(true);
-  const [showpageToggle, setShowpageToggle] = useState(true);
-
-  function toggleSidebar() {
-    setSidebarToggle((prev) => (prev = !prev));
-    console.log("sidebar toggled, state:", !sidebarToggle);
-  }
-
-  function toggleShowpage() {
-    setShowpageToggle((prev) => (prev = !prev));
-    console.log("showpage toggled, state:", !showpageToggle);
-  }
-
-  const functions = {
-    toggleSidebar,
-    toggleShowpage,
-  };
-
   console.log("App render");
-
   return (
     <div className="main">
-      <DataContext.Provider value={functions}>
+      <TogglesContextProvider>
         <Header icons={icons} />
-        <div className="content-wrapper">
-          {sidebarToggle && <Sidebar icons={icons} />}
-          {showpageToggle && <ShowPage icons={icons} />}
-        </div>
+        <Content icons={icons} />
         <Footer />
-      </DataContext.Provider>
+      </TogglesContextProvider>
     </div>
   );
 }
---
+++ src/components/Showpage/Content.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { TogglesContext } from "../../contexts/TogglesContext";
+import Sidebar from "../Sidebar/Sidebar";
+import ShowPage from "../Showpage/ShowPage";
+
+const Content = ({ icons }) => {
+  const contextValue = useContext(TogglesContext);
+  const { toggleSidebar, toggleShowpage, sidebarToggle, showpageToggle } =
+    useContext(TogglesContext);
+  console.log("Content Render");
+  return (
+    <div className="content-wrapper">
+      {sidebarToggle && <Sidebar icons={icons} />}
+      {showpageToggle && <ShowPage icons={icons} />}
+    </div>
+  );
+};
+
+export default Content;
