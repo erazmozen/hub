b9c075e5ba637b71dd27b349c2db22a01bd1d5bf
erazmozen <erazmozen@protonmail.com>
2022-12-01 13:39:03 +0100

    Switch context

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,19 @@
 import { useContext } from "react";
+import { TogglesContext } from "../../contexts/TogglesContext";
 import Clock from "./comps/Clock";
-import DataContext from "../../contexts/DataContext";
 import "./header.css";
 
 const Header = ({ icons }) => {
-  const { toggleSidebar, toggleShowpage } = useContext(DataContext);
-
+  const value = useContext(TogglesContext);
   console.log("Header render");
   return (
     <div className="header-wrapper card">
       <div className="common-icons-wrapper">
-        <button onClick={toggleSidebar}>
+        <button onClick={value.toggleSidebar}>
           <icons.BsToggles />
         </button>
 
-        <button onClick={toggleShowpage}>
+        <button onClick={value.toggleShowpage}>
           <icons.BsReverseLayoutTextSidebarReverse />
         </button>
         <a href="www.linkedin.com/in/erazmozen">
