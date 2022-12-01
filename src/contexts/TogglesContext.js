1efe4761ca5547c0ea1cb5fdc7ad254b8399628b
erazmozen <erazmozen@protonmail.com>
2022-12-01 13:37:52 +0100

    Create TogglesContext.js

---
+++ src/contexts/TogglesContext.js
@@ -0,0 +1,30 @@
+import { createContext, useState } from "react";
+
+const TogglesContext = createContext(null);
+
+const TogglesContextProvider = ({ children }) => {
+  const [sidebarToggle, setSidebarToggle] = useState(true);
+  const [showpageToggle, setShowpageToggle] = useState(true);
+  console.log("Context");
+
+  function toggleSidebar() {
+    setSidebarToggle((prev) => (prev = !prev));
+    console.log("sidebar toggled, state:", !sidebarToggle);
+  }
+
+  function toggleShowpage() {
+    setShowpageToggle((prev) => (prev = !prev));
+    console.log("showpage toggled, state:", !showpageToggle);
+  }
+
+  const value = {
+    toggleSidebar,
+    toggleShowpage,
+    sidebarToggle,
+    showpageToggle,
+  };
+
+  return <TogglesContext.Provider value={value}>{children}</TogglesContext.Provider>;
+};
+
+export { TogglesContext, TogglesContextProvider };
