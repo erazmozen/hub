f4b5b287c4a667a24f7431c12cabed7aba640cf6
erazmozen <erazmozen@protonmail.com>
2022-12-07 22:36:00 +0100

    Added icons provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./main.css";
-import icons from "./icons";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { TogglesContextProvider } from "./contexts/TogglesContext";
 import Content from "./components/Showpage/Content";
+import { IconsContextProvider } from "./contexts/IconsContext";
 
 function App() {
   console.log("App render");
   return (
     <div className="main">
-      <TogglesContextProvider>
-        <Header icons={icons} />
-        <Content icons={icons} />
-        <Footer />
-      </TogglesContextProvider>
+      <IconsContextProvider>
+        <TogglesContextProvider>
+          <Header />
+          <Content />
+          <Footer />
+        </TogglesContextProvider>
+      </IconsContextProvider>
     </div>
   );
 }
