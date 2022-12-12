3f9ebf07fa6e369feddf511dccad0aea0268a297
erazmozen <erazmozen@protonmail.com>
2022-12-12 16:15:34 +0100

    Reorder sidebar content and add new todo applet

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,28 @@
-import { useRef, useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { TodoProvider } from "../../contexts/TodosContext";
 import { TogglesContext } from "../../contexts/TogglesContext";
 import { IconsContext } from "../../contexts/IconsContext";
-import Todo from "./Todo/Todo";
+import SidebarLinks from "./SidebarLinks";
 import Button from "../common/Button";
 import "./sidebar.css";
+import TodoApplet from "./TodosApplet/TodosApplet";
 
 const Sidebar = () => {
   const { IoMdClose } = useContext(IconsContext);
-  const searchInput = useRef("");
   const value = useContext(TogglesContext);
 
   console.log("Sidebar render");
   return (
     <div className="sidebar-wrapper card">
-      <div className="sidebar-top">
-        <div className="search-wrapper">
-          <input
-            className="common-input"
-            ref={searchInput}
-            placeholder="Search"
-          />
-          <Button
-            onClick={value.toggleSidebar}
-            icon={<IoMdClose />}
-          />
-        </div>
-        <Todo searchInput={searchInput} />
-        <div className="router-links">
-          <Link className="common-button" to="/hub">
-            Portfolio
-          </Link>
-          <Link className="common-button" to="/hub/applets">
-            Applets
-          </Link>
-        </div>
-      </div>
+      <Button
+        onClick={value.toggleSidebar}
+        icon={<IoMdClose />}
+        title="Close sidebar"
+      />
+      <SidebarLinks />
+      <TodoProvider>
+        <TodoApplet />
+      </TodoProvider>
     </div>
   );
 };
