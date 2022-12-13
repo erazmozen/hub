ababc80d1f284080d441d47a7a142dde85ec0ea7
erazmozen <erazmozen@protonmail.com>
2022-12-13 21:08:34 +0100

    Add links to header

--- src/components/Showpage/comps/Showpage Header/ShowpageHeader.jsx
+++ src/components/Showpage/comps/Showpage Header/ShowpageHeader.jsx
@@ -1,21 +1,25 @@
 import { useContext } from "react";
 import { IconsContext } from "../../../../contexts/IconsContext";
+import SidebarLinks from "../../../Sidebar/SidebarLinks";
 import "./showpageheader.css";
 
 const ShowpageHeader = () => {
   const { CgProfile, FiSettings } =
     useContext(IconsContext);
   return (
-    <div className="showpageheader-wrapper">
-      <div className="showpageheader-info">
-        <CgProfile size={100} />
-        <div>
-          <h2>Dusan Mitic, 24</h2>
-          <p>Frontend Web Developer</p>
+    <>
+      <div className="showpageheader-wrapper">
+        <div className="showpageheader-info">
+          <CgProfile size={100} />
+          <div>
+            <h2>Dusan Mitic, 24</h2>
+            <p>Frontend Web Developer</p>
+          </div>
         </div>
+        <FiSettings className="showpageheader-settings" />
       </div>
-      <FiSettings className="showpageheader-settings" />
-    </div>
+      <SidebarLinks />
+    </>
   );
 };
 
