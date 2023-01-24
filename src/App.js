612e88f657926bf54deba1d7857743a440de1221
erazmozen <erazmozen@protonmail.com>
2023-01-24 16:12:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./main.css";
 import { TogglesContextProvider } from "./contexts/TogglesContext";
 import { IconsContextProvider } from "./contexts/IconsContext";
 import Header from "./components/Header/Header";
-import Content from "./components/Showpage/Content";
 import Footer from "./components/Footer/Footer";
+import Content from "./components/Content/Content";
 
 function App() {
   console.log("App render");
